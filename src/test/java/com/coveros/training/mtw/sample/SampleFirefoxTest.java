2d6f69c2e449921badcee71138b5c879a774fd23
brian <brian.hicks@coveros.com>
2016-06-17 13:20:17 -0500

    Change to use MarionetteDriver

--- src/test/java/com/coveros/training/mtw/sample/SampleFirefoxTest.java
+++ src/test/java/com/coveros/training/mtw/sample/SampleFirefoxTest.java
@@ -5,9 +5,13 @@ import static org.junit.Assert.assertEquals;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.MarionetteDriver;
+
+import io.github.bonigarcia.wdm.MarionetteDriverManager;
 
 /**
  * Simple test class that verifies proper installation of MTW tools and
@@ -18,10 +22,13 @@ import org.openqa.selenium.firefox.FirefoxDriver;
  */
 public class SampleFirefoxTest {
 	private WebDriver driver;
-
+	@BeforeClass
+	public static void setupClass() {
+		MarionetteDriverManager.getInstance().setup();
+	}
 	@Before
 	public void setUp() throws Exception {
-		driver = new FirefoxDriver();
+		driver = new MarionetteDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 
